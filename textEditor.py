5ba379ddbbd78b61f270465932a18f7af921dcd4
Mohamed Iyed Mansour <iyedmansour0@gmail.com>
2021-02-14 15:55:50 +0100

    Added open file function

--- textEditor.py
+++ textEditor.py
@@ -23,5 +23,11 @@ def saveAs():
     t = text.get(0.0, END)
     try:
         f.write(t.rstrip())
-        except:
-            showerror(title="Oops!", Message="Unable to save file...")
+        except:  showerror(title="Oops!", Message="Unable to save file...")
+
+def openFile():
+    f = askopenfile(mode='r')
+    t = f.reaf()
+    text.delete(0.0, END)
+    text.insert(0.0, t)
+
